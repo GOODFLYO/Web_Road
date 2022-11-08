87bf121f26e6b56e7c7d11325c6de21ee4052de5
goodfly <2084813108@qq.com>
2022-11-08 20:54:05 +0800

    addmyexe

--- 05express服务框架/server.js
+++ 05express服务框架/server.js
@@ -3,11 +3,17 @@ const express = require("express");
 //创建应用对象
 const app = express();
 //创建路由规则
-app.get("/server", (request, response) => {
+app.get("/", (request, response) => {
   //设置响应头 允许跨域
   response.setHeader('Access-Control-Allow-Origin','*');
+  
   //设置响应
-  response.send("hello zyn hello");
+  var mess = {
+    name: 'zyn',
+    sex: 'boy'
+  }
+  var m =   JSON.stringify(mess);
+  response.send(m);
 });
 //app监听端口启动服务
 app.listen(8000, () => {
