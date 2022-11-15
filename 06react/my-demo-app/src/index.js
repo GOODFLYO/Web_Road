c790121dc0eb4eb583d5298915caafc4443279fb
goodfly <2084813108@qq.com>
2022-11-15 19:12:03 +0800

    react

---
+++ 06react/my-demo-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const title = (
+  <h1>hello react!!!</h1>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(title);
+// ReactDOM.render(title,document.getElementById('root'))
