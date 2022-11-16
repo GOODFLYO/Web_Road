3d33b0e3ff9837784abbdddfb1545dddfa14e55a
goodfly <2084813108@qq.com>
2022-11-16 21:32:11 +0800

    20221216

--- 06react/my-demo-app/src/index.js
+++ 06react/my-demo-app/src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../src/index.css"
 
-const title = (
-  <h1>hello react!!!</h1>
-)
+const isloading = true
+const loaddata = ()=>{
+  if(isloading){
+    return <div>loading...</div>
+  }
+  return <div>数据加载完成。。。</div>
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(title);
+class APP extends React.Component{
+  // var a;
+  handleClick(){
+    
+    // console.log(a++,'单击事件触发了')
+    // console.log(a)
+  }
+
+  render(){
+    return(
+      <button onMouseEnter={this.handleClick}>DIANWO</button>
+    )
+  }
+}
+
+//这是一个map
+const songs = [
+  {id: 1,_name: 'zyn'},
+  {id: 2,_name: 'cc'}
+]
+  
+
+
+// const title = (
+//   <h1 className="title">
+//     条件渲染：
+//     {<APP/>}
+    
+//   </h1>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(title);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<APP/>);
 // ReactDOM.render(title,document.getElementById('root'))
